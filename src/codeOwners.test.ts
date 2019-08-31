af37832c66c2dc43c33dc99ddd1c777c8e1f0831
Quinn Slack <qslack@qslack.com>
2019-08-31 00:56:27 -0700

    chore: use assert instead of expect for tests

--- src/codeOwners.test.ts
+++ src/codeOwners.test.ts
@@ -1,19 +1,22 @@
-import expect from 'expect'
+import assert from 'assert'
 import { formatCodeOwners } from './codeOwners'
 
 describe('formatCodeOwners', () => {
-    it('formats null', () => expect(formatCodeOwners(null)).toEqual({ label: null, description: null }))
+    it('formats null', () => assert.deepStrictEqual(formatCodeOwners(null), { label: null, description: null }))
     it('formats empty', () =>
-        expect(formatCodeOwners([])).toEqual({ label: 'No owner', description: 'File has no code owner' }))
+        assert.deepStrictEqual(formatCodeOwners([]), { label: 'No owner', description: 'File has no code owner' }))
     it('formats 1', () =>
-        expect(formatCodeOwners(['alice'])).toEqual({ label: 'Owner: alice', description: 'Code owner: alice' }))
+        assert.deepStrictEqual(formatCodeOwners(['alice']), {
+            label: 'Owner: alice',
+            description: 'Code owner: alice',
+        }))
     it('formats 1 with long name', () =>
-        expect(formatCodeOwners(['alice-long-name'])).toEqual({
+        assert.deepStrictEqual(formatCodeOwners(['alice-long-name']), {
             label: 'Owner',
             description: 'Code owner: alice-long-name',
         }))
     it('formats 2', () =>
-        expect(formatCodeOwners(['alice', 'bob'])).toEqual({
+        assert.deepStrictEqual(formatCodeOwners(['alice', 'bob']), {
             label: '2 owners',
             description: 'Code owners: alice, bob',
         }))
