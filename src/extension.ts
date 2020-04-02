151135bc6326d0b8bb236daaee1e57ea92d43224
Felix Becker <felix.b@outlook.com>
2020-04-02 14:10:12 +0200

    chore: adopt ESLint

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
 import { combineLatest, from, Observable } from 'rxjs'
-import { startWith } from 'rxjs/operators'
+import { startWith, switchMap, filter, map } from 'rxjs/operators'
 import * as sourcegraph from 'sourcegraph'
 import { getCodeOwners } from './codeownersFile'
 import { formatCodeOwners } from './codeOwners'
 
 export function activate(ctx: sourcegraph.ExtensionContext): void {
     ctx.subscriptions.add(
-        combineLatest(
+        combineLatest([
             from(sourcegraph.workspace.onDidOpenTextDocument).pipe(
                 startWith(
                     sourcegraph.app.activeWindow && sourcegraph.app.activeWindow.activeViewComponent
@@ -14,25 +14,28 @@ export function activate(ctx: sourcegraph.ExtensionContext): void {
                         : null
                 )
             ),
-            new Observable(subscriber => sourcegraph.configuration.subscribe(() => subscriber.next()))
-        ).subscribe(async ([doc]) => {
-            if (!doc) {
-                return
-            }
-
-            let owners: string[] | null = null
-            if (!sourcegraph.configuration.get().value['codeOwnership.hide']) {
-                try {
-                    owners = await getCodeOwners(doc.uri)
-                } catch (err) {
-                    console.error(`Error getting code owners for ${doc.uri}:`, err)
-                }
-            }
-            const { label, description } = formatCodeOwners(owners)
-            sourcegraph.internal.updateContext({
-                [`codeOwnership.file.${doc.uri}.label`]: label,
-                [`codeOwnership.file.${doc.uri}.description`]: description,
+            new Observable(subscriber => sourcegraph.configuration.subscribe(() => subscriber.next())),
+        ])
+            .pipe(
+                map(([textDocument]) => textDocument),
+                filter((textDocument): textDocument is NonNullable<typeof textDocument> => !!textDocument),
+                switchMap(async textDocument => {
+                    if (!sourcegraph.configuration.get().value['codeOwnership.hide']) {
+                        try {
+                            return { textDocument, owners: await getCodeOwners(textDocument.uri) }
+                        } catch (err) {
+                            console.error(`Error getting code owners for ${textDocument.uri}:`, err)
+                        }
+                    }
+                    return { textDocument, owners: null }
+                })
+            )
+            .subscribe(({ textDocument, owners }) => {
+                const { label, description } = formatCodeOwners(owners)
+                sourcegraph.internal.updateContext({
+                    [`codeOwnership.file.${textDocument.uri}.label`]: label,
+                    [`codeOwnership.file.${textDocument.uri}.description`]: description,
+                })
             })
-        })
     )
 }
